ca46ebfe014ed626db82f6440676a086b826701a
Jakub Dworakowski <jdworakowski@clearpathrobotics.com>
2015-02-17 18:42:30 -0500

    Initial commit of statemachien

---
+++ sketch_feb17a/fsm/fsm.ino
@@ -0,0 +1,86 @@
+#include <FiniteStateMachine.h>
+
+State Start = State(start);
+State Calibrate = State(calibrate);
+State GoToRamp = State(goToRamp);
+State GoUpRamp = State(goUpRamp);
+State CorrectTraj = State(correctTraj);
+State GoDownRamp = State(goDownRamp);
+State Search = State(search);
+State GoToLegoMan = State(goToLegoMan);
+State PickUp = State(pickUp);
+State GoHome = State(goHome);
+State Chill = State(chill);
+
+FSM snr = FSM(Start); // search and rescue state machine
+
+void setup()
+{
+  // We setup all our sensors up in this bitch.
+}
+
+void loop()
+{
+  /**
+  * In this block we will transition from one state to another
+  * For example we can switch from the start state to the calibrate state like so
+  * snr.transitionTo(calibrate); 
+  * then we update the state machine so it properly transitions
+  * snr.update();
+  */
+}
+
+void start()
+{
+  // Do something like wait for nudge or button clicked
+}
+
+void calibrate()
+{
+  // Figure out where we might be relative to somewhere
+}
+
+void goToRamp()
+{
+  // based on calculated trajectory go towards ramp
+}
+
+void goUpRamp()
+{
+  // go up the ramp
+}
+
+void correctTraj()
+{
+  // correct ramp trajectory
+}
+
+void goDownRamp()
+{
+  // go down the ramp
+}
+
+void search()
+{
+  // search for the lego man
+}
+
+void goToLegoMan()
+{
+  // go to the lego man
+}
+
+void pickUp()
+{
+  // pick up the lego man
+}
+
+void goHome()
+{
+  // returning to home base
+}
+
+void chill()
+{
+  // chill at base
+}
