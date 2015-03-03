a206b835878d2d111e001f36a17dc82f7b06a4d2
Jakub Dworakowski <jdworakowski@uwaterloo.ca>
2015-03-02 21:28:23 -0500

    Motor drivers.

---
+++ sketch_feb17a/fsm/Motor.cpp
@@ -0,0 +1,34 @@
+/**
+ * Motor.cpp
+ */
+#include "Arduino.h"
+#include "Motor.h"
+
+Motor::Motor(int forwardPin, int reversePin, int enablePin) : 
+                                   forward_pin_(forwardPin), 
+                                   reverse_pin_(reversePin),
+                                     enable_pin_(enablePin)
+{
+  pinMode(forward_pin_, OUTPUT);
+  pinMode(reverse_pin_, OUTPUT);
+  pinMode(enable_pin_, OUTPUT);
+ 
+  digitalWrite(enable_pin_, HIGH);
+}
+
+void Motor::setVelocity(unsigned int speed, bool direction)
+{
+   analogWrite(enable_pin_, speed);
+   
+   if(direction)
+   {
+   	  digitalWrite(forward_pin_, HIGH);
+   	  digitalWrite(reverse_pin_, LOW);
+   }
+   else
+   {
+   	  digitalWrite(forward_pin_, LOW);
+   	  digitalWrite(reverse_pin_, HIGH);
+   }
+
+}
---
+++ sketch_feb17a/fsm/Motor.h
@@ -0,0 +1,24 @@
+/**
+ * Look at me I'm a header file
+ *
+ * Motor.h
+ */
+#ifndef Motor_h
+#define Motor_h
+
+#include "Arduino.h"
+class Motor
+{
+public:
+  /**
+   * Constructor for the light intensity sensor
+   */
+  Motor(int forwardPin, int reversePin, int enablePin);
+  void setVelocity(unsigned int speed, bool direction);
+
+
+private:
+  int forward_pin_, reverse_pin_, enable_pin_;
+};
+
+#endif
