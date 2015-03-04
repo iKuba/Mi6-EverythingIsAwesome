92c22be563ec4e2cf351358774fd3a6d67b23e90
Jakub Dworakowski <jdworakowski@uwaterloo.ca>
2015-03-03 22:51:23 -0500

    Added timer interrupt to fsm.ino used to enter the calculation state

--- sketch_feb17a/fsm/fsm.ino
+++ sketch_feb17a/fsm/fsm.ino
@@ -5,6 +5,8 @@
 #include "PinDefinitions.h"
 #include "NineDOF.h"
 #include "Wire.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 
 void start()
@@ -64,7 +66,7 @@ void chill()
 
 void doCalculations()
 {
-  
+
 }
 
 State Start = State(start);
@@ -95,13 +97,38 @@ TEMT6000 reciever_right = TEMT6000(RECEIVER_RIGHT);
 
 NineDOF ndof = NineDOF();
 
+// This variable is used to transition into the calculation state
+bool calculateNav = false;
+
 void setup()
 {
   Serial.begin (9600);
   Wire.begin();
   ndof.setup();
 
+  // initialize Timer1
+  cli();          // disable global interrupts
+  TCCR1A = 0;     // set entire TCCR1A register to 0
+  TCCR1B = 0;     // same for TCCR1B
+
+  // set compare match register to desired timer count:
+  OCR1A = 15624;
+  // turn on CTC mode:
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler:
+  TCCR1B |= (1 << CS10);
+  TCCR1B |= (1 << CS12);
+  // enable timer compare interrupt:
+  TIMSK1 |= (1 << OCIE1A);
+  // enable global interrupts:
+  sei();
+
   // We setup all our sensors up in this bitch.
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+    calculateNav = !calculateNav;
 }
 
 void loop()
