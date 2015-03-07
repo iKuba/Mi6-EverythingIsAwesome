c1502404a6620cae0c9870d4383d2e093df85895
Jakub Dworakowski <jdworakowski@uwaterloo.ca>
2015-03-07 13:24:10 -0500

    Using Timer2 instead of Timer1 to avoid conflicts in Servo library.

--- sketch_feb17a/fsm/fsm.ino
+++ sketch_feb17a/fsm/fsm.ino
@@ -5,9 +5,7 @@
 #include "PinDefinitions.h"
 #include "NineDOF.h"
 #include "Wire.h"
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
+#include "MsTimer2.h"
 
 void start()
 {
@@ -112,23 +110,7 @@ void setup()
   Serial.begin (9600);
   Wire.begin();
   ndof.setup();
-
-  // initialize Timer1
-  cli();          // disable global interrupts
-  TCCR1A = 0;     // set entire TCCR1A register to 0
-  TCCR1B = 0;     // same for TCCR1B
-
-  // set compare match register to desired timer count:
-  OCR1A = 15624;
-  // turn on CTC mode:
-  TCCR1B |= (1 << WGM12);
-  // Set CS10 and CS12 bits for 1024 prescaler:
-  TCCR1B |= (1 << CS10);
-  TCCR1B |= (1 << CS12);
-  // enable timer compare interrupt:
-  TIMSK1 |= (1 << OCIE1A);
-  // enable global interrupts:
-  sei();
+  MsTimer2::set(500, timerInterrupt);
 
   attachInterrupt(2, killSwitch, CHANGE);
 
@@ -141,7 +123,7 @@ void killSwitch()
 }
 
 
-ISR(TIMER1_COMPA_vect)
+void timerInterrupt()
 {
     calculateNav = true;
 }
