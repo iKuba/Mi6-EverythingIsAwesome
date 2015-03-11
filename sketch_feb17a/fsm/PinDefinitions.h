a6bfba0474d86932232556998b7626183930ebe2
Yu Mei <yumei@v1020-wn-115-14.campus-dynamic.uwaterloo.ca>
2015-03-10 20:28:40 -0400

    Updated pin definitions with most up to date pin mapping

--- sketch_feb17a/fsm/PinDefinitions.h
+++ sketch_feb17a/fsm/PinDefinitions.h
@@ -7,35 +7,44 @@
 
 
 // motor control
-#define MOTOR_LEFT_SPEED 1
-#define MOTOR_RIGHT_SPEED 5
+#define MOTOR_LEFT_SPEED 2
+#define MOTOR_RIGHT_SPEED 7
 
-#define MOTOR_LEFT_DIR 55
-#define MOTOR_RIGHT_DIR 56
+#define MOTOR_LEFT_DIR_A 3
+#define MOTOR_RIGHT_DIR_A 5
+#define MOTOR_LEFT_DIR_B 4
+#define MOTOR_RIGHT_DIR_B 6
+
+// brush
+#define MOTOR_BRUSH_SPEED 8
+// prop
+#define PROP_ESC_SPEED 9
 
 // limit switch input
 #define LIMIT_SWITCH_1 52
-#define LIMIT_SWITCH_2 53
-#define LIMIT_SWITCH_3 54
+#define LIMIT_SWITCH_2 50
+#define LIMIT_SWITCH_3 48
 
 // sonar
-#define SONAR_TRIG1 70
-#define SONAR_TRIG2 71
-#define SONAR_TRIG3 72
+#define SONAR_TRIG1 46
+#define SONAR_TRIG2 44
+#define SONAR_TRIG3 42
 
-#define SONAR_ECHO1 82
-#define SONAR_ECHO2 83
-#define SONAR_ECHO3 84
+#define SONAR_ECHO1 0 //Analogue Pins
+#define SONAR_ECHO2 1 //Analogue Pins
+#define SONAR_ECHO3 2 //Analogue Pins
 
 // encoders
-#define ENCODER_RIGHT 50
-#define ENCODER_LEFT 51
+#define ENCODER_RIGHT_A 53 
+#define ENCODER_LEFT_A 49
+#define ENCODER_RIGHT_B 51
+#define ENCODER_LEFT_B 47
 
-// emmiter/receiver
-#define EMITTER_LEFT 57
-#define EMMITER_RIGHT 58
+// IR
+#define IR_TRIG 40
+#define IR_RECEIVE 3 //Analogue Pins
 
-#define RECEIVER_LEFT 59
-#define RECEIVER_RIGHT 60
+// flex Lego Sensor
+#define FLEX_SENSOR 4 //Analogue Pins
 
-#define KILL_SWITCH 21
+#define KILL_SWITCH 22
