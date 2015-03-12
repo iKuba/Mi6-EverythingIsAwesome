ed6423e5bfc43ce27efcb749cdf86c3c4ca3d213
Yu Mei <yumei@v1020-wn-115-14.campus-dynamic.uwaterloo.ca>
2015-03-12 16:13:54 -0400

    updated states  (pseudo code) for search state

--- sketch_feb17a/fsm/fsm.ino
+++ sketch_feb17a/fsm/fsm.ino
@@ -31,10 +31,18 @@ void goUpRamp()
   //Check 9DOF to check angle
     //if not back away and re enter ramp
     //if yes
-      //Check Accelerametor to see if guides are down
+      //Check Accelerametor on guides to see if guides are down
         //if not turn robot until both guide accelerameter indicate it is vertical
         //if it is procedd witj turning prop on and ramping of prop speed
         // once speed reached proceed with climbing of ramp
+          //check if guides are moving up 
+          // if it is left
+            // turn left slightly util it is no longer up
+            //should see what kind of correction is needed to be straight on the ramp
+          // if it is right 
+            // turn right slightly until it is no longer up
+            //should see what kind of correction is needed to be straight on the ramp
+          // if not procceed forward
 }
 
 void correctTraj()
@@ -44,17 +52,51 @@ void correctTraj()
 
 void goDownRamp()
 {
+  //this state will happen realy fast since we are sliding down ramp fast
   // go down the ramp
   //Check 9DOF to check angle
     //if not keep moving forward until correct angle
     //if yes
-    //stop driving and stop prop
+    //drive in reverse and stop prop immediately
     // option to check guide accelerameter to ensure still on ramp.
 }
 
 void search()
 { 
   // search for the lego man
+  // drive arround in determined path via path planning using ultrasonics
+  // use ultrasound and encoders to track where we are
+  // once path have been determined check limit switchs to see if hit base
+  // limit siwthc adjsut logic
+
+    //if all three hit flush with base 
+    // climb base mobe to next state
+
+    //if left limit hit car need to turn to the right until all three hit
+    // could be 3 possible situations
+      // 1 - car is aligned to base but only the left side 
+      // 1 - and care need to shift to the left
+      // 2 - car is not aligned and at a angle such that the left side of the car is on base and right is not
+      // 2 - we can check 9dof sensor to see orientation and adjust by turn left (Different from shifting right)
+      // 2 - until all three limit is triggered and car is angled
+      // 3 - if only left and right hit we are at corner then back off move to one direction and try again
+      // 3 - then back off a certain distance
+      // 3 - turn right or left 45 degrees
+      // 3 - move forward a known distance
+      // 3 - turn 90 degrees the opposite of what did previously
+      // 3 - move forward to see if all three limit will trigger and repeat.
+
+    //in right limit hit car need to turn to the left until all three hit
+      // same with only left
+
+    // if left and center or right and center hit
+      // if left and center car need to be shifted to the left/
+        // plan path via back off a certain known distance
+        // turn 90 degree left
+        // move a certain known distance
+        // turn right 90 degree right
+        // try again
+
 }
 
 void goToLegoMan()
