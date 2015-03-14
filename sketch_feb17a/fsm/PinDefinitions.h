56ae2b9d685711a18e18a1b88a7e2c8a90248dfd
Yu Mei <yumei@v1020-wn-115-14.campus-dynamic.uwaterloo.ca>
2015-03-13 20:28:04 -0400

    Updated Pin Definition

--- sketch_feb17a/fsm/PinDefinitions.h
+++ sketch_feb17a/fsm/PinDefinitions.h
@@ -20,18 +20,18 @@
 #define PROP_ESC_SPEED 11
 
 // limit switch input
-#define LIMIT_SWITCH_1 52
-#define LIMIT_SWITCH_2 50
-#define LIMIT_SWITCH_3 48
+#define LIMIT_SWITCH_LEFT 52
+#define LIMIT_SWITCH_MID 50
+#define LIMIT_SWITCH_RIGHT 48
 
 // sonar
-#define SONAR_TRIG1 46
-#define SONAR_TRIG2 44
-#define SONAR_TRIG3 42
+#define SONAR_TRIG_LEFT 46
+#define SONAR_TRIG_CENTER 44
+#define SONAR_TRIG3_RIGHT 42
 
-#define SONAR_ECHO1 0 //Analogue Pins
-#define SONAR_ECHO2 1 //Analogue Pins
-#define SONAR_ECHO3 2 //Analogue Pins
+#define SONAR_ECHO_LEFT 0 //Analog Pins
+#define SONAR_ECHO_CENTER 1 //Analog Pins
+#define SONAR_ECHO_RIGHT 2 //Analog Pins
 
 // encoders
 #define ENCODER_RIGHT_A 2 //Inturrupt
@@ -41,16 +41,17 @@
 
 //9DOF Sensor
 #define DOF9_SERIAL_SCL 21 //i2c scl pin
-#define DOF9_SERIAL_SDA 20 //i2c sda pin
+#define DOF9_SERIAL_SDA 20 //i2c sda pin //Also Kill Switch Interrupt
 
 //Accel Sensor
-#define ACCEL_LEFT_GUIDE 5 //Analogue Pins
-#define ACCEL_LEFT_GUIDE 6 //Analogue Pins
+#define ACCEL_LEFT_GUIDE 5 //Analog Pins
+#define ACCEL_RIGHT_GUIDE 6 //Analog Pins
 
 // IR
-#define IR_SIGNAL 3 //Analogue Pins
+#define IR_SIGNAL 3 //Analog Pins
 
 // Flex Lego Sensor
-#define FLEX_SENSOR 4 //Analogue Pins
+#define FLEX_SENSOR 4 //Analog Pins
 
 #define KILL_SWITCH 20 //Inturrupt
+#define START_BUTTON 30 //Incase we need it
